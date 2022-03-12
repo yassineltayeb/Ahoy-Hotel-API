a75931b31afa54cee9a78bd01612267ae98a3ef2
Yassin <yassineltayeb@live.com>
2022-03-12 23:26:29 +0400

    elasticsearch

---
+++ Ahoy-Hotel-API/Elasticsearch/ElasticsearchClient.cs
@@ -0,0 +1,54 @@
+﻿using Ahoy_Hotel_API.Models;
+using Elasticsearch.Net;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ahoy_Hotel_API.Elasticsearch;
+
+public class ElasticsearchClient
+{
+    //private readonly ILogger<ElasticsearchClient> logger;
+    //private readonly IElasticClient client;
+    //private readonly string indexName;
+
+    //public ElasticsearchClient(ILogger<ElasticsearchClient> logger, IOptions<ElasticsearchOptions> options)
+    //    : this(logger, CreateClient(options.Value.Uri), options.Value.IndexName)
+    //{
+
+    //}
+
+    //public ElasticsearchClient(ILogger<ElasticsearchClient> logger, IElasticClient client, string indexName)
+    //{
+    //    this.logger = logger;
+    //    this.indexName = indexName;
+    //    this.client = client;
+    //}
+
+    //public async Task<CreateIndexResponse> CreateIndexAsync(CancellationToken cancellationToken)
+    //{
+    //    var createIndexResponse = await client.Indices.CreateAsync(indexName, descriptor =>
+    //    {
+    //        return descriptor.Map<ElasticsearchHotel>(mapping => mapping
+    //            .Properties(properties => properties
+    //                .Text(textField => textField.Name(document => document.Id))
+    //                .Text(textField => textField.Name(document => document.Name))
+    //                .Text(textField => textField.Name(document => document.Address))
+    //                .Text(textField => textField.Name(document => document.Location))
+    //                .Text(textField => textField.Name(document => document.Description))
+    //                .Text(textField => textField.Name(document => document.Email))
+    //                .Text(textField => textField.Name(document => document.PhoneNumber))
+    //    }), cancellationToken);
+
+    //    if (logger.IsDebugEnabled())
+    //    {
+    //        logger.LogDebug($"CreateIndexResponse DebugInformation: {createIndexResponse.DebugInformation}");
+    //    }
+
+    //    return createIndexResponse;
+    //}
+}
---
+++ Ahoy-Hotel-API/Models/ElasticsearchHotel.cs
@@ -0,0 +1,12 @@
+﻿namespace Ahoy_Hotel_API.Models
+{
+    public class ElasticsearchHotel : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
